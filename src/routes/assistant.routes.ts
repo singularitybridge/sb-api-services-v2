b66939135fdd2c6b95dcd1bac6a14816472346d4
Avi Osipov <avi@open-college.co.il>
2023-12-26 22:30:51 +0200

    Update Assistant model and routes

--- src/routes/assistant.routes.ts
+++ src/routes/assistant.routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import { getJob, getJobs, rerunJob } from '../services/agenda/agenda.service';
 import { handleUserInput } from '../services/assistant.service';
+import { Assistant } from '../models/Assistant';
 
 const assistantRouter = express.Router();
 
@@ -8,6 +9,44 @@ assistantRouter.post('/user-input', async (req, res) => {
   const { userInput, assistantId, threadId } = req.body;
   const response = await handleUserInput(userInput, assistantId, threadId);
   res.send(response);
+});
+
+assistantRouter.get('/', async (req, res) => {
+  const assistants = await Assistant.find({});
+  res.send(assistants);
+});
+
+assistantRouter.get('/:id', async (req, res) => {
+  const assistant = await Assistant.findById(req.params.id);
+  res.send(assistant);
+});
+
+assistantRouter.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const assistantData = req.body;
+  const assistant = await Assistant.findByIdAndUpdate(id, assistantData, { new: true, upsert: true });
+  res.send(assistant);
+});
+
+assistantRouter.post('/', async (req, res) => {
+  const assistantData = req.body;
+  const newAssistant = new Assistant(assistantData);
+  try {
+    await newAssistant.save();
+    res.send(newAssistant);
+  } catch (err) {
+    if (err instanceof Error && 'code' in err && err.code === 11000) {
+      res.status(400).send({ message: 'Duplicate key error: an assistant with this phone number already exists.' });
+    } else {
+      res.status(500).send({ message: 'An error occurred while trying to create the assistant.' });
+    }
+  }
+});
+
+assistantRouter.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  await Assistant.findByIdAndDelete(id);
+  res.send({ message: 'Assistant deleted successfully' });
 });
 
 export { assistantRouter };
