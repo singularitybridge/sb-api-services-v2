894d5fdcbb0d4776ee5750145b6ac4a9782c6136
Avi.O <aviosipov@gmail.com>
2024-01-19 12:24:39 +0200

    Refactor inbox service and model

--- src/models/Inbox.ts
+++ src/models/Inbox.ts
@@ -2,6 +2,8 @@ import mongoose, { Document, Schema } from 'mongoose';
 
 export interface IInbox extends Document {
   sessionId: mongoose.Schema.Types.ObjectId;
+  // as assistant may change during a session, we need to store the assistantId
+  assistantId: mongoose.Schema.Types.ObjectId;
   message: string;
   created: Date;
 }
@@ -10,6 +12,11 @@ const InboxSchema: Schema = new Schema({
   sessionId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Session',
+    required: true,
+  },
+  assistantId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Assistant',
     required: true,
   },
   message: {
--- src/services/inbox.service.ts
+++ src/services/inbox.service.ts
@@ -2,6 +2,8 @@ import moment from 'moment';
 import { IInbox, Inbox } from '../models/Inbox';
 import { Session } from '../models/Session';
 import { User } from '../models/User';
+import { Assistant } from '../models/Assistant';
+import mongoose from 'mongoose';
 
 interface IInboxInput {
   message: string;
@@ -9,30 +11,84 @@ interface IInboxInput {
 }
 
 export const addMessageToInbox = async (inboxInput: IInboxInput) => {
+
   const inboxMessage = new Inbox(inboxInput);
+  const session = await Session.findById(inboxInput.sessionId);
+  const assistant = await Assistant.findById(session?.assistantId);
+
+  if (!assistant) {
+    throw new Error('Session not found');
+  }
+  
+  inboxMessage.assistantId = assistant._id;
   return await inboxMessage.save();
+
 };
 
 export const getInboxMessages = async (companyId: string) => {
+  const aggregationPipeline = [
+    {
+      $match: {
+        sessionId: {
+          $in: (await Session.find({ companyId }).select('_id').lean()).map(s => s._id)
+        }
+      }
+    },
+    {
+      $lookup: {
+        from: 'sessions',
+        localField: 'sessionId',
+        foreignField: '_id',
+        as: 'sessionInfo'
+      }
+    },
+    { $unwind: '$sessionInfo' },
+    {
+      $lookup: {
+        from: 'users',
+        localField: 'sessionInfo.userId',
+        foreignField: '_id',
+        as: 'userInfo'
+      }
+    },
+    { $unwind: '$userInfo' },
+    {
+      $lookup: {
+        from: 'assistants',
+        localField: 'sessionInfo.assistantId',
+        foreignField: '_id',
+        as: 'assistantInfo'
+      }
+    },
+    { $unwind: '$assistantInfo' },
+    {
+      $group: {
+        _id: '$sessionId',
+        messages: {
+          $push: {
+            _id: '$_id',
+            message: '$message',
+            createdAt: { $toDate: '$created' },
+            userName: '$userInfo.name',
+            sessionActive: '$sessionInfo.active',
+            assistantName: '$assistantInfo.name',
+            assistantId: '$assistantInfo._id'
+          }
+        }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        sessionId: '$_id',
+        messages: 1
+      }
+    }
+  ];
 
-  const sessions = await Session.find({ companyId });
-  const sessionIds = sessions.map(session => session._id.toString());
-  const messages = await Inbox.find({ sessionId: { $in: sessionIds } });
-
-  const inboxMessages = await Promise.all(messages.map(async (message) => {
-    const session = await Session.findById(message.sessionId);
-    const user = await User.findById(session?.userId);
-
-    return {
-      _id: message._id,
-      message: message.message,
-      createdAt: moment(message.created).fromNow(),
-      userName: user?.name,
-    };
-  }));
-
-  return inboxMessages;
+  return Inbox.aggregate(aggregationPipeline);
 };
+
 
 export const getInboxMessage = async (id: string) => {
   return Inbox.findById(id);
