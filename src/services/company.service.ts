fa8cd33394f1c4c6a8c6e10b8650659d291aa4e7
rivkyschon <rivkyschon@outlook.co.il>
2024-02-26 13:32:15 +0200

    Modify company service to create default assistant

--- src/services/company.service.ts
+++ src/services/company.service.ts
@@ -1,13 +1,12 @@
 import { Assistant } from '../models/Assistant';
 import { Company, ICompany } from '../models/Company';
-
+import { createAssistant } from '../services/oai.assistant.service';
 
 export const createCompany = async (companyData: ICompany) => {
-
   const company = new Company(companyData);
   await company.save();
 
-  const assistant = new Assistant({
+  const defaultAssistantData = {
     companyId: company._id,
     assistantId: 'default',
     name: `${company.name} Assistant`,
@@ -16,14 +15,35 @@ export const createCompany = async (companyData: ICompany) => {
     voice: 'Polly.Emma',
     language: 'en-US',
     llmModel: 'gpt-3.5-turbo-1106',
-    llmPrompt: `This is the default assistant for ${company.name}. It was created automatically when the company was created.`
-  });
+    llmPrompt: `This is the default assistant for ${company.name}. It was created automatically when the company was created.`,
+  };
+  const newAssistant = new Assistant(defaultAssistantData);
 
-  await assistant.save();
-  return company;
+  try {
+    await newAssistant.save();
+    const openAIAssistant = await createAssistant(
+      defaultAssistantData.name,
+      defaultAssistantData.description,
+      defaultAssistantData.llmModel,
+      defaultAssistantData.llmPrompt,
+    );
 
+    newAssistant.assistantId = openAIAssistant.id;
+    await newAssistant.save();
+
+    return company;
+  } catch (err) {
+    if (err instanceof Error && 'code' in err && err.code === 11000) {
+      throw new Error(
+        'Duplicate key error: an assistant with this phone number already exists.',
+      );
+    } else {
+      throw new Error(
+        `An error occurred while trying to create the assistant : ${err}`,
+      );
+    }
+  }
 };
-
 
 export const getCompany = async (id: string) => {
   return Company.findById(id);
@@ -49,4 +69,4 @@ export const deleteCompany = async (id: string) => {
     throw new Error('Company not found');
   }
   return company;
-}
+};
