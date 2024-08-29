d8956728499799851892f798ef6f129115c69eb1
avi.o <aviosipov@gmail.com>
2024-08-29 16:01:11 +0300

    ```text feat: Update PhotoRoom service to include companyId in removeBackground
    This commit updates the PhotoRoom service to include the companyId parameter in the removeBackground function. This change allows for better organization and management of company-specific API keys and ensures that the correct API key is used for each company. It also aligns with the recent addition of the 'photoroom_api_key' to the company settings.

--- src/routes/photoroom.routes.ts
+++ src/routes/photoroom.routes.ts
@@ -1,16 +1,19 @@
 import express from 'express';
+import { validateApiKeys } from '../services/api.key.service';
+import { AuthenticatedRequest } from '../middleware/auth.middleware';
 import { photoRoomService } from '../services/photoroom.service';
 
 const photoRoomRouter = express.Router();
 
-photoRoomRouter.post('/remove-background', async (req, res) => {
+photoRoomRouter.post('/remove-background', validateApiKeys(['photoroom']), express.json(), async (req: AuthenticatedRequest, res) => {
   try {
     const { imageUrl } = req.body;
+
     if (!imageUrl) {
       return res.status(400).json({ error: 'Image URL is required' });
     }
 
-    const processedImage = await photoRoomService.removeBackground(imageUrl);
+    const processedImage = await photoRoomService.removeBackground(req.company._id, imageUrl);
     res.set('Content-Type', 'image/png');
     res.send(processedImage);
   } catch (error) {
--- src/services/photoroom.service.ts
+++ src/services/photoroom.service.ts
@@ -1,16 +1,21 @@
 import https from 'https';
-import fs from 'fs';
+import { getCompany } from './company.service';
 
 class PhotoRoomService {
-  private apiKey: string;
   private apiUrl: string;
 
   constructor() {
-    this.apiKey = '59113086e3977b60c8c5a0fbb0dae1c50c6a36b4';
     this.apiUrl = 'https://image-api.photoroom.com/v2/edit';
   }
 
-  async removeBackground(imageUrl: string): Promise<Buffer> {
+  async removeBackground(companyId: string, imageUrl: string): Promise<Buffer> {
+    const company = await getCompany(companyId);
+    const apiKey = company.api_keys.find((key: { key: string; value: string }) => key.key === 'photoroom_api_key')?.value;
+
+    if (!apiKey) {
+      throw new Error('PhotoRoom API key not found for the company');
+    }
+
     const editParams = 'background.color=transparent&background.expandPrompt=never&background.scaling=fill&outputSize=1000x1000&padding=0.1';
     const options = {
       hostname: 'image-api.photoroom.com',
@@ -18,7 +23,7 @@ class PhotoRoomService {
       path: `/v2/edit?${editParams}&imageUrl=${encodeURIComponent(imageUrl)}`,
       method: 'GET',
       headers: {
-        'x-api-key': this.apiKey
+        'x-api-key': apiKey
       }
     };
 
