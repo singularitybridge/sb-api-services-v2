408e6b82d05a374edc1c1e81a802bd7867015926
avi.o <aviosipov@gmail.com>
2024-09-19 09:08:59 +0300

    refactor: Add content and contentType actions to index.ts and update action-discovery.service.ts

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
-/// file_path: /src/services/oai.assistant.service.ts
+/// file_path: /src/actions/index.ts
 
 export * from './types';
 export * from './factory';
 export * from './inboxActions';
 export * from './assistantActions';
 export * from './calendarActions';
+export * from './contentActions';
+export * from './contentTypeActions';
