84dbe196e65fed162aa35d6620b7d9ba54bbfd01
avi.o <aviosipov@gmail.com>
2024-09-22 23:40:55 +0300

    Refactor file paths for integrations

--- src/actions/index.ts
+++
@@ -1,7 +0,0 @@
-/// file_path: /src/actions/index.ts
-
-export * from './inboxActions';
-export * from './assistantActions';
-export * from './calendarActions';
-export * from './contentActions';
-export * from './contentTypeActions';
